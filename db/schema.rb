b01babfaf9d4ab0c7e6806478e09ba79288328c2
TechMura <mura.sk.katu@gmail.com>
2020-12-02 12:06:09 +0900

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_021426) do
+ActiveRecord::Schema.define(version: 2020_12_02_011001) do
 
   create_table "objectives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "declaration", null: false
