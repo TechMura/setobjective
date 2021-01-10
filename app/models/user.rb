b6beaa15bee6ef2f0b830992df5b2d4e1d01624a
TechMura <mura.sk.katu@gmail.com>
2021-01-10 17:02:47 +0900

    ユーザー登録バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true, length: {maximum: 8}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  validates :password, length: { minimum: 6 }
+
   has_many  :objectives
   has_many  :menus
 end
