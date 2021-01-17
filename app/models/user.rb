01716d28f5868c05f72e9bb0d56d71c728cfbcbb
TechMura <mura.sk.katu@gmail.com>
2021-01-17 17:29:42 +0900

    ログインのエラーメッセージ表示実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable
+
   validates :nickname, presence: true, length: {maximum: 8}
 
   devise :database_authenticatable, :registerable,
