a961daaaaa23d427c6c4aaf0f5ad91408ee1f562
TechMura <mura.sk.katu@gmail.com>
2021-03-14 10:51:08 +0900

    目標設定・編集を部分テンプレート

--- app/javascript/delete_menu.js
+++ app/javascript/delete_menu.js
@@ -2,6 +2,7 @@
 function deleteMenu(){
   const deleteBtns = Array.from(document.getElementsByClassName("delete-btn"));
   deleteBtns.forEach(function(deleteBtn){
+    // 削除ボタンはhiddenで隠しているからそれを取り除く
     deleteBtn.removeAttribute("class");
   });
 };
