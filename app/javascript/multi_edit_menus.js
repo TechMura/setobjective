df50f11a023c4e32822d79f5af1496a0ac42a722
TechMura <mura.sk.katu@gmail.com>
2021-03-16 22:19:28 +0900

    非同期でフォームを送信

--- app/javascript/multi_edit_menus.js
+++ app/javascript/multi_edit_menus.js
@@ -3,7 +3,15 @@ function multiEditMenu(){
   // const deleteBtns = Array.from(document.getElementsByClassName("delete-btn"));
   // deleteBtns.forEach(function(deleteBtn){
 
-  // const
+  const submit = document.getElementById('submit')
+  submit.addEventListener('click',function(e){
+    const formData = new FormData(document.getElementById("menu-form"));
+    formData.append('objective_id', )
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/menus/add_update", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
 
   // });
 };
