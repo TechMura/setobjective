7c277ad8785a7f8f2f3b2b1eda39ad035c89dd80
TechMura <mura.sk.katu@gmail.com>
2021-03-17 22:50:05 +0900

    フォームの要素の取得を追加

--- app/javascript/multi_edit_menus.js
+++ app/javascript/multi_edit_menus.js
@@ -3,17 +3,55 @@ function multiEditMenu(){
   // const deleteBtns = Array.from(document.getElementsByClassName("delete-btn"));
   // deleteBtns.forEach(function(deleteBtn){
 
-  const submit = document.getElementById('submit')
+  const submit = document.getElementById('add-btn')
   submit.addEventListener('click',function(e){
-    const formData = new FormData(document.getElementById("menu-form"));
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/menus/add_update", true);
-    XHR.responseType = "json";
-    XHR.send(formData);
+
+
+    // formとエラーメッセージの要素取得
+    const weekDayContents = document.getElementById("menu_week_id");
+    const todoContents = document.getElementById("menu_todo");
+    const timeContents = document.getElementById("menu_time");
+    const weekTableData = document.getElementsByClassName("menu-list");
+    const alertWeekDayComment = document.getElementById("alert-week-comment");
+    const alertTodoComment = document.getElementById("alert-todo-comment");
+
+    // バリデーション
+    if (weekDayContents.value == "" && todoContents.value == ""){
+      alertWeekDayComment.classList.contains("hidden") ? alertWeekDayComment.classList.remove("hidden"): "" ;
+      alertTodoComment.classList.contains("hidden") ? alertTodoComment.classList.remove("hidden"): "" ;
+      return
+    }
+    else if (weekDayContents.value == ""){
+      alertWeekDayComment.classList.contains("hidden") ? alertWeekDayComment.classList.remove("hidden") : "";
+      !alertTodoComment.classList.contains("hidden") ? alertTodoComment.classList.add("hidden") : "";
+      return
+    }
+    else if (todoContents.value == ""){
+      !alertWeekDayComment.classList.contains("hidden") ? alertWeekDayComment.classList.add("hidden"): "";
+      alertTodoComment.classList.contains("hidden") ? alertTodoComment.classList.remove("hidden"):"";
+      return
+    }
+
+    else if (weekDayContents.value != "" && todoContents.value != ""){
+      !alertWeekDayComment.classList.contains("hidden") ? alertWeekDayComment.classList.add("hidden"): "";
+      !alertTodoComment.classList.contains("hidden") ? alertTodoComment.classList.add("hidden"):"";
+      const formData = new FormData(document.getElementById("menu-form"));
+      const XHR = new XMLHttpRequest();
+      XHR.open("POST", "/menus/add_update", true);
+      XHR.responseType = "json";
+      XHR.send(formData);
+      XHR.onload = function(){
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;
+        }
+        const item = this.response.menu
+        
+      };
+    }
   });
 
-  // });
-};
+}
 
 function deleteMenu(){
 
@@ -23,5 +61,5 @@ function deleteMenu(){
 window.addEventListener('load',function(){
   if (document.URL.match(/multi_edit/)){
     multiEditMenu();
-  };
+  }
 });
